1d56f60d5d39e4e2897c4656c241dba46487e819
Classicclowwn <114720751+Classicclowwn@users.noreply.github.com>
2023-04-20 15:02:17 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,13 +2,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.botname = "✘CLASSICCLOWN✘" 
-global.botLanguage = "Eglish" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "MOYIN"  
-global.packname = "CLASSIC✯CLOWN✯BOT"  
+global.botname = "Queen Anya" 
+global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
+global.author = "𝙋𝙄𝙆𝘼"  
+global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
 global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "✘CLASSICCLOWN✘" // Your own watermark 
-global.headerText = "✘CLASSICCLOWN✘ by Moyin" // text for header of menu         
+global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
+global.headerText = "Anya by Pika"" // text for header of menu         
 global.DocumentTitle = "MASTER⚔️PIKA" 
 
 
@@ -16,20 +16,20 @@ global.ownername = "Moyin"   // Owner name //
 global.ownernumber = ['2349139098641'] 
 global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
-global.continent = "Africa" // your continent name 
-global.region = "Nigeria" // your country name
-global.state = "oyo" // your state name
-global.district = "ibadan" // your district name
-global.timezone = "GMT/UTC + 1h" // search on google if you don't know the timezone of your country //
-global.instagramId = "_classicclowwn" // Your insta ID
-global.email = "classicclowwn@gmail.com" // put your email here
+global.continent = "Asia" // your continent name 
+global.region = "India" // your country name
+global.state = "Assam" // your state name
+global.district = "Dibrugarh" // your district name
+global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
+global.instagramId = "3.69_pika" // Your insta ID
+global.email = "example@exmaple.com" // put your email here
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
 global.premium = ['916900******','917355******']  // available soon
 global.sudo = ['2348163243259']  // Co - Owners //
 
-global.prefa = ['.']
+global.prefa = ['-']
 
 //--------Random bot name for Info section--------\\
 name16 = "𓄂⍣⃝ 🇦𝚗𝚢𝚊 𓆩×͜×𓆪ꪾ️"
